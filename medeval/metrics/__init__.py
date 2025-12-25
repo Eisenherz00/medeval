f2cc76d76fa15c7e5b66b0ae718156a4e98cd10d
ZhuoyaangLi <96129270+ZhuoyaangLi@users.noreply.github.com>
2025-12-25 13:00:43 +0100

    init

--- medeval/metrics/__init__.py
+++ medeval/metrics/__init__.py
@@ -1,5 +1,26 @@
 """Metrics for medical imaging evaluation."""
 
+from medeval.metrics.classification import (
+    accuracy,
+    adaptive_expected_calibration_error,
+    auprc,
+    auroc,
+    balanced_accuracy,
+    brier_score_classification,
+    cohen_kappa,
+    compute_classification_metrics,
+    decision_curve,
+    expected_calibration_error,
+    f1_score_metric,
+    group_by_patient,
+    mcc,
+    reliability_diagram,
+    sensitivity,
+    specificity,
+    threshold_adaptive_calibration_error,
+    youden_threshold,
+)
+
 from medeval.metrics.segmentation import (
     average_symmetric_surface_distance,
     brier_score,
@@ -17,22 +38,40 @@ from medeval.metrics.segmentation import (
 )
 
 __all__ = [
-    # Overlap metrics
+    # Segmentation metrics
     "dice_score",
     "jaccard_index",
     "precision_score",
     "recall_score",
     "volumetric_similarity",
-    # Surface metrics
     "hausdorff_distance",
     "hausdorff_distance_95",
     "average_symmetric_surface_distance",
     "surface_dice",
+    "soft_dice_score",
+    "brier_score",
+    "threshold_sweep",
+    "compute_segmentation_metrics",
+    # Classification metrics
+    "auroc",
+    "auprc",
+    "accuracy",
+    "balanced_accuracy",
+    "sensitivity",
+    "specificity",
+    "f1_score_metric",
+    "mcc",
+    "cohen_kappa",
     # Calibration metrics
-   "soft_dice_score",
-   "brier_score",
-   "threshold_sweep",
-   # Comprehensive function
-   "compute_segmentation_metrics",
+    "expected_calibration_error",
+    "adaptive_expected_calibration_error",
+    "threshold_adaptive_calibration_error",
+    "brier_score_classification",
+    "youden_threshold",
+    "reliability_diagram",
+    "decision_curve",
+    # Utility functions
+    "group_by_patient",
+    "compute_classification_metrics",
 ]
 
